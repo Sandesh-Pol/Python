b58fafe5d277a5afccd9878163da69db6b4aa735
Sandesh-Pol <sandeshpol268@gmail.com>
2023-08-10 20:15:26 +0530

    all

---
+++ OOP concepts/2.py
@@ -0,0 +1,4 @@
+import BankSystem as bs
+import demo.demo1 as d1
+
+d1.helllo()
--- OOP concepts/BankSystem.py
+++ OOP concepts/BankSystem.py
@@ -1,21 +1,23 @@
-class BankSystem:
-    def __init__(self, b):
-        self.bal = b
-        
-    def withdraw(self, amount):  
-        if amount <= self.bal:   
-            self.bal = self.bal - amount
+
+# def __init__(self, b):
+#         self.bal = b
+bal = 1000
+def withdraw( amount):  
+        if amount <= bal:   
+            bal = bal - amount
             print("Withdraw amount {} successful . . . !".format(amount))
-            print("Remaining Balance: " + str(self.bal)) 
+            print("Remaining Balance: " + str(bal)) 
             
         else:
             print("You don't have enough balance . . .!")
             
-    def deposit(self, amount):  
-        self.bal = self.bal + amount
+def deposit( amount):  
+        bal = bal + amount
         print("Deposit amount {} successful . . . !".format(amount))
-        print("Your current balance: " + str(self.bal))  
+        print("Your current balance: " + str(bal))  
         
-p1 = BankSystem(1000)
+# p1 = BankSystem(1000)
 
-p1.withdraw(100)  
+def hello():
+      print("hello")
+# p1.withdraw(100)  
